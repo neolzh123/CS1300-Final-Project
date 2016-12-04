9048614a6822ea151f0a3ff94a9ab6af9abd6738
Your Name <you@example.com>
2016-12-04 16:33:28 -0500

    Fred project update 1

--- js/music_image_gallery.js
+++ js/music_image_gallery.js
@@ -1,6 +1,15 @@
 /*Global variables defined before functions*/
 var index = 0;
-var gallery = ["music_eems.jpg", "music_alter.jpg", "music_riley_owens.jpg", "music_sunny_weather.jpg", "wailin_brass_band.jpg"];
+var gallery = ["https://www.youtube.com/embed/EtM0LNQ-pMo", "https://www.youtube.com/embed/CftfIwFDr9w", "https://www.youtube.com/embed/GEHOe5PtL7s", "https://www.youtube.com/embed/7lBtJauMnyM", "https://www.youtube.com/embed/oQ0Ej0Rtc64"];
+var apple_festival = document.getElementById('music_video');
+var buttons = document.getElementById("buttons").getElementsByTagName("span"); 
+
+/*These videos were found from 
+    - https://www.youtube.com/watch?v=EtM0LNQ-pMo
+    - https://www.youtube.com/watch?v=CftfIwFDr9w 
+    - https://www.youtube.com/watch?v=GEHOe5PtL7s 
+    - https://www.youtube.com/watch?v=7lBtJauMnyM 
+    - https://www.youtube.com/watch?v=oQ0Ej0Rtc64
 var apple_festival = document.getElementById('apple_festival');
 var buttons = document.getElementById("buttons").getElementsByTagName("span"); 
 
@@ -20,10 +29,10 @@ function myNext(){
     if (index < gallery.length - 1) {
         buttons[index + 1].className = "on";
         index += 1;
-        apple_festival.src = 'images/music/' + gallery[index];
+        apple_festival.src = gallery[index];
     }else{
         index = 0;
-        apple_festival.src = 'images/music/' + gallery[index];
+        apple_festival.src = gallery[index];
         buttons[0].className = "on";
     }
 }
@@ -40,10 +49,10 @@ function myPrev(){
     if (index > 0) {
         buttons[index - 1].className = "on";
         index -= 1;
-        apple_festival.src = 'images/' + gallery[index];
+        apple_festival.src = gallery[index];
     }else{
         index = gallery.length - 1;
-        apple_festival.src = 'images/' + gallery[index];
+        apple_festival.src = gallery[index];
         buttons[gallery.length - 1].className = "on";
     }
 }
